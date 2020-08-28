cc1d216aee8b0a1372a438faf8192292301531d8
Asahi Takenouchi <le-soleil-levant.0802@outlook.com>
2020-08-28 19:41:35 +0900

    fix: use promise

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
-import gpio from 'rpi-gpio'
+import { promise as gpio } from 'rpi-gpio'
 
-const main = () => {
-  const pin = 8
+const PIN = 8
+
+const main = async () => {
+  console.log('Setup has started')
+  await gpio.setup(PIN, gpio.DIR_OUT).catch((err) => console.error(err))
+  console.log('Setup has finished')
+
   let isOn = false
-  gpio.setup(pin, gpio.DIR_OUT, () => {
-    console.log('Setup finished')
-    setInterval(() => {
-      if (isOn) {
-        console.log('LED is ON')
-        gpio.write(pin, true)
-        isOn = false
-      } else {
-        console.log('LED is OFF')
-        gpio.write(pin, false)
-        isOn = true
-      }
-    }, 1000)
-  })
+  setInterval(() => {
+    if (isOn) {
+      console.log('LED is ON')
+      gpio.write(PIN, true)
+      isOn = false
+    } else {
+      console.log('LED is OFF')
+      gpio.write(PIN, false)
+      isOn = true
+    }
+  }, 1000)
 }
 
 main()
